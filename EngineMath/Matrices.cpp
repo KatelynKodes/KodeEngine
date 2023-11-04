2151b5b108b774b1e84d0a0d943877e982229480
KatelynWest <katelynwest225@gmail.com>
2023-11-04 17:01:58 -0500

    Made Matrix4 its own struct, added the identity method to both structs
    For some reason operator methods will not go over to the CCP file.

--- EngineMath/Matrices.cpp
+++ EngineMath/Matrices.cpp
@@ -1,5 +1,6 @@
 #include "Matrices.h"
 
+// MATRIX 3
 EngineMath::Matrix3::Matrix3()
 {
 	_a1 = 0.0f, _a2 = 0.0f, _a3 = 0.0f;
@@ -14,24 +15,20 @@ EngineMath::Matrix3::Matrix3(float A1, float A2, float A3, float B1, float B2, f
 	_c1 = C1, _c2 = C2, _c3 = C3;
 }
 
+//MATRIX 4
+
 EngineMath::Matrix4::Matrix4()
 {
-	//Call Matrix3 constructor
-	Matrix3();
-
-	_a4 = 0.0f;
-	_b4 = 0.0f;
-	_c4 = 0.0f;
-
+	_a1 = 0.0f, _a2 = 0.0f, _a3 = 0.0f, _a4 = 0.0f;
+	_b1 = 0.0f, _b2 = 0.0f, _b3 = 0.0f, _b4 = 0.0f;
+	_c1 = 0.0f, _c2 = 0.0f, _c3 = 0.0f, _c4 = 0.0f;
 	_d1 = 0.0f, _d2 = 0.0f, _d3 = 0.0f, _d4 = 0.0f;
 }
 
 EngineMath::Matrix4::Matrix4(float A1, float A2, float A3, float A4, float B1, float B2, float B3, float B4, float C1, float C2, float C3, float C4, float D1, float D2, float D3, float D4)
 {
-	Matrix3(A1, A2, A3,
-			B1, B2, B3,
-			C1, C2, C3);
-
-	_a4 = A4, _b4 = B4, _c4 = C4;
+	_a1 = A1, _a2 = A2, _a3 = A3, _a4 = A4;
+	_b1 = B1, _b2 = B2, _b3 = B3, _b4 = B4;
+	_c1 = C1, _c2 = C2, _c3 = C3, _c4 = C4;
 	_d1 = D1, _d2 = D2, _d3 = D3, _d4 = D4;
 }
--- EngineMath/Matrices.h
+++ EngineMath/Matrices.h
@@ -2,20 +2,31 @@
 
 namespace EngineMath
 {
-	class Matrix3
+	struct Matrix3
 	{
 	public:
 		Matrix3();
 		Matrix3(float A1, float A2, float A3,
 				float B1, float B2, float B3,
 				float C1, float C2, float C3);
+
+		static Matrix3 Identity() {
+			return Matrix3(1, 0, 0,
+						   0, 1, 0,
+						   0, 0, 1);
+		}
+
+		Matrix3 operator +(Matrix3 rhs);
+		Matrix3 operator -(Matrix3 rhs);
+		Matrix3 operator *(Matrix3 rhs);
+
 	protected:
 		float _a1, _a2, _a3,
 			  _b1, _b2, _b3,
 			  _c1, _c2, _c3;
 	};
 
-	class Matrix4 : EngineMath:: Matrix3
+	struct Matrix4
 	{
 	public:
 		Matrix4();
@@ -23,8 +34,18 @@ namespace EngineMath
 				float B1, float B2, float B3, float B4,
 				float C1, float C2, float C3, float C4,
 				float D1, float D2, float D3, float D4);
+
+		static Matrix4 Identity() {
+			return Matrix4(1, 0, 0, 0,
+						   0, 1, 0, 0,
+						   0, 0, 1, 0,
+						   0, 0, 0, 1);
+		}
+
 	private:
-		float _a4, _b4, _c4,
+		float _a1, _a2, _a3, _a4, 
+			  _b1, _b2, _b3, _b4, 
+			  _c1, _c2, _c3, _c4,
 			  _d1, _d2, _d3, _d4;
 	};
 }
