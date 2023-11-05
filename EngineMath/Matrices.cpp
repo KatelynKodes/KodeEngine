e354ca17d168a0c5a59fbd347aed665efab1492c
KatelynWest <katelynwest225@gmail.com>
2023-11-05 16:48:49 -0600

    Added operator methods

--- EngineMath/Matrices.cpp
+++ EngineMath/Matrices.cpp
@@ -15,6 +15,39 @@ EngineMath::Matrix3::Matrix3(float A1, float A2, float A3, float B1, float B2, f
 	_c1 = C1, _c2 = C2, _c3 = C3;
 }
 
+EngineMath::Matrix3 EngineMath::Matrix3::operator+(Matrix3 rhs)
+{
+	return EngineMath::Matrix3(this->_a1 + rhs._a1, this->_a2 + rhs._a2, this->_a3 + rhs._a3,
+							   this->_b1 + rhs._b1, this->_b2 + rhs._b2, this->_b3 + rhs._b3,
+							   this->_c1 + rhs._c1, this->_c2 + rhs._c2, this->_c3 + rhs._c3);
+}
+
+EngineMath::Matrix3 EngineMath::Matrix3::operator-(Matrix3 rhs)
+{
+	return EngineMath::Matrix3(this->_a1 - rhs._a1, this->_a2 - rhs._a2, this->_a3 - rhs._a3,
+							   this->_b1 - rhs._b1, this->_b2 - rhs._b2, this->_b3 - rhs._b3,
+							   this->_c1 - rhs._c1, this->_c2 - rhs._c2, this->_c3 - rhs._c3);
+}
+
+EngineMath::Matrix3 EngineMath::Matrix3::operator*(Matrix3 rhs)
+{
+	float a1 = (this->_a1 * rhs._a1) + (this->_a2 * rhs._b1) + (this->_a3 * rhs._c1);//Column 1
+	float a2 = (this->_a1 * rhs._a2) + (this->_a2 * rhs._b2) + (this->_a3 * rhs._c2); //Column 2
+	float a3 = (this->_a1 * rhs._a3) + (this->_a2 * rhs._b3) + (this->_a3 * rhs._c3); //Column 3
+
+	float b1 = (this->_b1 * rhs._a1) + (this->_b2 * rhs._b1) + (this->_b3 * rhs._c1);//Column 1
+	float b2 = (this->_b1 * rhs._a2) + (this->_b2 * rhs._b2) + (this->_b3 * rhs._c2); //Column 2
+	float b3 = (this->_b1 * rhs._a3) + (this->_b2 * rhs._b3) + (this->_b3 * rhs._c3); //Column 3
+
+	float c1 = (this->_c1 * rhs._a1) + (this->_c2 * rhs._b1) + (this->_c3 * rhs._c1);//Column 1
+	float c2 = (this->_c1 * rhs._a2) + (this->_c2 * rhs._b2) + (this->_c3 * rhs._c2); //Column 2
+	float c3 = (this->_c1 * rhs._a3) + (this->_c2 * rhs._b3) + (this->_c3 * rhs._c3); //Column 3
+
+	return EngineMath::Matrix3(a1, a2, a3,
+							   b1, b2, b3,
+							   c1, c2, c3);
+}
+
 //MATRIX 4
 
 EngineMath::Matrix4::Matrix4()
@@ -31,4 +64,48 @@ EngineMath::Matrix4::Matrix4(float A1, float A2, float A3, float A4, float B1, f
 	_b1 = B1, _b2 = B2, _b3 = B3, _b4 = B4;
 	_c1 = C1, _c2 = C2, _c3 = C3, _c4 = C4;
 	_d1 = D1, _d2 = D2, _d3 = D3, _d4 = D4;
+}
+
+EngineMath::Matrix4 EngineMath::Matrix4::operator+(EngineMath::Matrix4 rhs)
+{
+	return EngineMath::Matrix4(this->_a1 + rhs._a1, this->_a2 + rhs._a2, this->_a3 + rhs._a3, this->_a4 + rhs._a4,
+							   this->_b1 + rhs._b1, this->_b2 + rhs._b2, this->_b3 + rhs._b3, this->_b4 + rhs._a4,
+							   this->_c1 + rhs._c1, this->_c2 + rhs._c2, this->_c3 + rhs._c3, this->_c4 + rhs._c4,
+							   this->_d1 + rhs._d1, this->_d2 + rhs._d2, this->_d3 + rhs._d3, this->_d4 + rhs._d4);
+}
+
+EngineMath::Matrix4 EngineMath::Matrix4::operator-(EngineMath::Matrix4 rhs)
+{
+	return EngineMath::Matrix4(this->_a1 - rhs._a1, this->_a2 - rhs._a2, this->_a3 - rhs._a3, this->_a4 - rhs._a4,
+							   this->_b1 - rhs._b1, this->_b2 - rhs._b2, this->_b3 - rhs._b3, this->_b4 - rhs._a4,
+		                       this->_c1 - rhs._c1, this->_c2 - rhs._c2, this->_c3 - rhs._c3, this->_c4 - rhs._c4,
+							   this->_d1 - rhs._d1, this->_d2 - rhs._d2, this->_d3 - rhs._d3, this->_d4 - rhs._d4);
+}
+
+EngineMath::Matrix4 EngineMath::Matrix4::operator*(EngineMath::Matrix4 rhs)
+{
+	float a1 = (this->_a1 * rhs._a1) + (this->_a2 * rhs._b1) + (this->_a3 * rhs._c1) + (this->_a4 * rhs._d1); //Column 1
+	float a2 = (this->_a1 * rhs._a2) + (this->_a2 * rhs._b2) + (this->_a3 * rhs._c2) + (this->_a4 * rhs._d2); //Column 2
+	float a3 = (this->_a1 * rhs._a3) + (this->_a2 * rhs._b3) + (this->_a3 * rhs._c3) + (this->_a4 * rhs._d3); //Column 3
+	float a4 = (this->_a1 * rhs._a4) + (this->_a2 * rhs._b4) + (this->_a3 * rhs._c4) + (this->_a4 * rhs._d4); //Column 4
+
+	float b1 = (this->_b1 * rhs._a1) + (this->_b2 * rhs._b1) + (this->_b3 * rhs._c1) + (this->_b4 * rhs._d1); //Column 1
+	float b2 = (this->_b1 * rhs._a2) + (this->_b2 * rhs._b2) + (this->_b3 * rhs._c2) + (this->_b4 * rhs._d2); //Column 2
+	float b3 = (this->_b1 * rhs._a3) + (this->_b2 * rhs._b3) + (this->_b3 * rhs._c3) + (this->_b4 * rhs._d3); //Column 3
+	float b4 = (this->_b1 * rhs._a4) + (this->_b2 * rhs._b4) + (this->_b3 * rhs._c4) + (this->_b4 * rhs._d4); //Column 4
+
+	float c1 = (this->_c1 * rhs._a1) + (this->_c2 * rhs._b1) + (this->_c3 * rhs._c1) + (this->_c4 * rhs._d1); //Column 1
+	float c2 = (this->_c1 * rhs._a2) + (this->_c2 * rhs._b2) + (this->_c3 * rhs._c2) + (this->_c4 * rhs._d2); //Column 2
+	float c3 = (this->_c1 * rhs._a3) + (this->_c2 * rhs._b3) + (this->_c3 * rhs._c3) + (this->_c4 * rhs._d3); //Column 3
+	float c4 = (this->_c1 * rhs._a4) + (this->_c2 * rhs._b4) + (this->_c3 * rhs._c4) + (this->_c4 * rhs._d4); //Column 4
+
+	float d1 = (this->_d1 * rhs._a1) + (this->_d2 * rhs._b1) + (this->_d3 * rhs._c1) + (this->_d4 * rhs._d1); //Column 1
+	float d2 = (this->_d1 * rhs._a2) + (this->_d2 * rhs._b2) + (this->_d3 * rhs._c2) + (this->_d4 * rhs._d2); //Column 2
+	float d3 = (this->_d1 * rhs._a3) + (this->_d2 * rhs._b3) + (this->_d3 * rhs._c3) + (this->_d4 * rhs._d3); //Column 3
+	float d4 = (this->_d1 * rhs._a4) + (this->_d2 * rhs._b4) + (this->_d3 * rhs._c4) + (this->_d4 * rhs._d4); //Column 4
+
+	return EngineMath::Matrix4(a1, a2, a3, a4,
+							   b1, b2, b3, b4,
+							   c1, c2, c3, c4,
+							   d1, d2, d3, c4);
 }
--- EngineMath/Matrices.h
+++ EngineMath/Matrices.h
@@ -16,9 +16,9 @@ namespace EngineMath
 						   0, 0, 1);
 		}
 
-		Matrix3 operator +(Matrix3 rhs);
-		Matrix3 operator -(Matrix3 rhs);
-		Matrix3 operator *(Matrix3 rhs);
+		EngineMath::Matrix3 operator +(Matrix3 rhs);
+		EngineMath::Matrix3 operator -(Matrix3 rhs);
+		EngineMath::Matrix3 operator *(Matrix3 rhs);
 
 	protected:
 		float _a1, _a2, _a3,
@@ -35,12 +35,17 @@ namespace EngineMath
 				float C1, float C2, float C3, float C4,
 				float D1, float D2, float D3, float D4);
 
-		static Matrix4 Identity() {
-			return Matrix4(1, 0, 0, 0,
+		static EngineMath::Matrix4 Identity() {
+			return EngineMath::Matrix4(1, 0, 0, 0,
 						   0, 1, 0, 0,
 						   0, 0, 1, 0,
 						   0, 0, 0, 1);
 		}
+
+		EngineMath::Matrix4 operator+(EngineMath::Matrix4 rhs);
+		EngineMath::Matrix4 operator-(EngineMath::Matrix4 rhs);
+		EngineMath::Matrix4 operator*(EngineMath::Matrix4 rhs);
+
 
 	private:
 		float _a1, _a2, _a3, _a4, 
